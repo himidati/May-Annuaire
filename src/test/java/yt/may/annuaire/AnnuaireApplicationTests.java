39fd0bde23d0eac031840b2f0d9e1138b73489b9
formation06 <formation06@natsystem.fr>
2026-05-27 17:20:55 +0200

    configuration projet, -> nistallation des dépendances

---
+++ src/test/java/yt/may/annuaire/AnnuaireApplicationTests.java
@@ -0,0 +1,15 @@
+package yt.may.annuaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class AnnuaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
